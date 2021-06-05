ba8510d6df81036fc50d3c740306310b78df5477
rajar <rajar@LAPTOP-4IBD932O>
2021-06-05 13:45:09 +0530

    mesg3

---
+++ com.app.TestProject/src/test/java/com/app/TestCases/AppTest.java
@@ -0,0 +1,16 @@
+package com.app.TestCases;
+
+import org.testng.annotations.Test;
+
+public class AppTest 
+{
+    
+    @Test
+    public void verifytest()
+    {
+        System.out.println("From test pkgggggggggggggg");
+        System.out.println("***********************************");
+        System.out.println("***********************************");
+        System.out.println("***********************************");
+        System.out.println("***********************************");
+}}
