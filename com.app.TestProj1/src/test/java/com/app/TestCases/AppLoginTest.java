c95d01665712c82086a17b3ec19c179b2c31a5b9
vnrvik <srdk1@yahoo.com>
2021-06-13 18:52:13 +0530

    newcommit1

--- com.app.TestProj1/src/test/java/com/app/TestCases/AppLoginTest.java
+++ com.app.TestProj1/src/test/java/com/app/TestCases/AppLoginTest.java
@@ -1,15 +1,16 @@
 package com.app.TestCases;
 
 import org.openqa.selenium.By;
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 
-public class AppTest 
+public class AppLoginTest 
 {
     public static WebDriver Dr;
-    public static String appurl="mycontactform.com";
+    //public static String appurl="mycontactform.com";
 	
 	
 	
@@ -22,8 +23,13 @@ public class AppTest
     	
 		System.setProperty("webdriver.chrome.driver", "C:\\ksr\\SeleniumJarFiles\\chromedriver.exe");
 		Dr=new ChromeDriver();
-		Dr.get(appurl);
+		Dr.navigate().to("http://www.mycontactform.com");
 		Dr.manage().window().maximize();
-    	
+		
+		Dr.findElement(By.name("user")).sendKeys("user1");
+		Dr.findElement(By.name("pass")).sendKeys("pwd1");
+		Dr.findElement(By.name("btnSubmit")).submit();
+    
+    
     }
 }
