b031d0cd2a8acb3a36e792460dbb20bc0da35944
vnrvik <srdk1@yahoo.com>
2021-07-12 19:00:43 +0530

    commit1111

--- com.app.TestProj1/src/test/java/com/app/TestCases/AppLoginTest.java
+++ com.app.TestProj1/src/test/java/com/app/TestCases/AppLoginTest.java
@@ -1,35 +1,54 @@
 package com.app.TestCases;
 
 import org.openqa.selenium.By;
+
+
 import java.util.concurrent.TimeUnit;
+
+import javax.annotation.Nonnull;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.app.Utilities.BrowserFactory;
+import com.app.Utilities.ConfigReader;
 
-public class AppLoginTest 
+public class AppLoginTest
 {
+    
+    public static BrowserFactory browser;
+	public static ConfigReader config;
     public static WebDriver Dr;
-    //public static String appurl="mycontactform.com";
+    
+	@BeforeSuite
+
+	public void setupsuite() {
+		
+		config=new ConfigReader();
+		browser=new BrowserFactory();
+		
+	}
 	
-	
+	@BeforeTest
+	public void launchbrowser() {
+		Dr = browser.getbrowser("chrome", "http://www.mycontactform.com");
+	}
 	
 	@Test
     public void verifytest()
     {
-    	System.out.println("hellooooooooooooooooooooooooooooooo");
-    	System.out.println("hellooooooooooooooooooooooooooooooo");
-    	System.out.println("hellooooooooooooooooooooooooooooooo");
-    	
-		System.setProperty("webdriver.chrome.driver", "C:\\ksr\\SeleniumJarFiles\\chromedriver.exe");
-		Dr=new ChromeDriver();
-		Dr.navigate().to("http://www.mycontactform.com");
-		Dr.manage().window().maximize();
 		
-		Dr.findElement(By.name("user")).sendKeys("user1");
-		Dr.findElement(By.name("pass")).sendKeys("pwd1");
-		Dr.findElement(By.name("btnSubmit")).submit();
-    
-    
     }
+
+	@AfterTest
+	
+	public void closebrowser() {
+		browser.closebrowser();
+	}
+
+
 }
